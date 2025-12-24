a0a0935703d7a9c70e1a152bfe7faf7fe18bab62
LorenaMamede <lorenamb@dcc.ufrj.br>
2025-12-24 00:13:56 -0300

    Documentando codigo

--- mini-project-1/src/models/vector_field.py
+++ mini-project-1/src/models/vector_field.py
@@ -4,6 +4,7 @@ import math
 
 
 class VectorField(nn.Module):
+    # loss: MSE
     """
     Parametriza dx/dt = f(x, t) usando rede neural.
     Args:
@@ -19,15 +20,20 @@ class VectorField(nn.Module):
         layers = []
         input_dim = features + time_embed_dim
 
+        # cria as camadas da rede neural
         for h in hidden_dims:
             layers.append(nn.Linear(input_dim, h))
             layers.append(nn.ReLU())
             input_dim = h
-
         final = nn.Linear(input_dim, features)
+
+        # altera os pesos da última camada com uma distribuição normal
         nn.init.normal_(final.weight, mean=0.0, std=0.01)
+
+        # inicializa o bias com zero
         nn.init.zeros_(final.bias)
 
+        # adiciona as camadas em uma rede sequencial
         layers.append(final)
         self.net = nn.Sequential(*layers)
 
@@ -39,21 +45,30 @@ class VectorField(nn.Module):
         Returns:
             (batch, time_embed_dim)
         """
+
+        # converte em tensor caso não seja
         if not torch.is_tensor(t):
             t = torch.tensor(t)
 
+        # converte em float e adiciona uma dimensao 
         t = t.float().unsqueeze(-1)
 
+        # calcula metade arredondada da dimensão de tempo
         half_dim = self.time_embed_dim // 2
 
+        # cria frequências logaritmicamente espacadas
+        # para diferenciar os embeddings e evitar repetição, ou seja,
+        # o embedding fica "injetivo"
         frequencies = torch.exp(
             -math.log(10000) * torch.arange(half_dim, dtype=torch.float32) / half_dim
         ).to(t.device)
 
         args = t * frequencies
 
+        # cria embbeding sinusoidal [sin(wt), cos(wt)]
         emb = torch.cat([torch.sin(args), torch.cos(args)], dim=-1)
-
+        
+        # adiciona uma dimensão extra para compensar caso seja par
         if self.time_embed_dim % 2 == 1:
             emb = nn.functional.pad(emb, (0, 1))
 
@@ -68,14 +83,20 @@ class VectorField(nn.Module):
         Returns:
             dx_dt: (batch, features)
         """
+
+        # converte t em tensor caso nao seja
         if not torch.is_tensor(t):
             t = torch.tensor(t, dtype=x.dtype, device=x.device)
 
+        # caso t seja escalar
         if t.ndim == 0:
             t = t.repeat(x.shape[0])
 
+        # gera o embedding
         t_emb = self.time_embedding(t)  
 
+        # concatena estados e tempo
         h = torch.cat([x, t_emb], dim=-1)
 
+        #retorna o resultado da rede
         return self.net(h)
